5c148558f0eb529f3977032b02a1329fdb8eecf1
Vicent_Chen <Vicent_Chen@yeah.net>
2021-10-11 11:29:56 +0800

    add morton code utils

--- HimeUtils/HimeMath.h
+++ HimeUtils/HimeMath.h
@@ -29,4 +29,41 @@ namespace Falcor
         r |= (v >> 1);
         return r;
     }
+
+    inline uint deinterleave_32bit(uint x)
+    {
+        x &= 0x09249249;                  // x = ---- 9--8 --7- -6-- 5--4 --3- -2-- 1--0
+        x = (x ^ (x >> 2)) & 0x030c30c3;  // x = ---- --98 ---- 76-- --54 ---- 32-- --10
+        x = (x ^ (x >> 4)) & 0x0300f00f;  // x = ---- --98 ---- ---- 7654 ---- ---- 3210
+        x = (x ^ (x >> 8)) & 0xff0000ff;  // x = ---- --98 ---- ---- ---- ---- 7654 3210
+        x = (x ^ (x >> 16)) & 0x000003ff; // x = ---- ---- ---- ---- ---- --98 7654 3210
+        return x;
+    }
+
+    inline uint3 deinterleave_uint3(uint x)
+    {
+        uint3 v;
+        v.z = deinterleave_32bit(x);
+        v.y = deinterleave_32bit(x >> 1);
+        v.x = deinterleave_32bit(x >> 2);
+        return v;
+    }
+
+    inline uint interleave_32bit(uint x)
+    {
+        x &= 0x000003ff;                  // x = ---- ---- ---- ---- ---- --98 7654 3210
+        x = (x ^ (x << 16)) & 0xff0000ff; // x = ---- --98 ---- ---- ---- ---- 7654 3210
+        x = (x ^ (x << 8)) & 0x0300f00f; // x = ---- --98 ---- ---- 7654 ---- ---- 3210
+        x = (x ^ (x << 4)) & 0x030c30c3; // x = ---- --98 ---- 76-- --54 ---- 32-- --10
+        x = (x ^ (x << 2)) & 0x09249249; // x = ---- 9--8 --7- -6-- 5--4 --3- -2-- 1--0
+        return x;
+    }
+
+    inline uint interleave_uint3(uint3 v)
+    {
+        uint vx = interleave_32bit(v.x);
+        uint vy = interleave_32bit(v.y);
+        uint vz = interleave_32bit(v.z);
+        return vx * 4 + vy * 2 + vz;
+    }
 }
--- HimeUtils/HimeUtils.cpp
+++ HimeUtils/HimeUtils.cpp
@@ -20,7 +20,6 @@ void HimeComputePassDesc::createComputePassIfNecessary(ComputePass::SharedPtr& p
     }
 }
 
-
 void HimeBufferHelpers::createAndCopyBuffer(Buffer::SharedPtr& pBuffer, uint elementSize, uint elementCount, const void* pCpuData, const std::string& bufferName)
 {
     assert(pCpuData);
